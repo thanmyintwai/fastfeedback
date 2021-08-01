078d3006d05b4c38d73634b402b6cd92da1cf531
thanmyintwai <thanmyintwai.my@googlemail.com>
2021-08-01 15:01:17 +1000

    show the key

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import { useAuth } from '../lib/auth'
 export default function Index() {
   const auth = useAuth()
-  return auth.user ? (
+  
+  return 
+    <>
+    <p>{process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}</p>
+  auth.user ? (
     <div>
-      <p>{process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}</p>
+      
       <p>Email: {auth.user.email}</p>
       <button onClick={(e) => auth.signout()}>Sign Out</button>
     </div>
   ) : (
     <button onClick={(e) => auth.signinWithGitHub()}>Sign In</button>
   )
+  </>
 }
