b912693faef15997f71cd7dd7eb85bbaee608397
thanmyintwai <thanmyintwai.my@googlemail.com>
2021-08-01 15:07:31 +1000

    looks ok

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,12 @@
 import { useAuth } from '../lib/auth'
 export default function Index() {
   const auth = useAuth()
-  
-  return (
-    <>
-    <p>{process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}</p>
-    {/* { auth.user ? ( )}
-    {auth.user} ? (
+  return auth.user ? (
     <div>
-      
       <p>Email: {auth.user.email}</p>
       <button onClick={(e) => auth.signout()}>Sign Out</button>
     </div>
   ) : (
     <button onClick={(e) => auth.signinWithGitHub()}>Sign In</button>
-  ) */}
-  </>
   )
 }
